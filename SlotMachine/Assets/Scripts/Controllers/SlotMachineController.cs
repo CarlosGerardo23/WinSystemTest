c5c95abb9fca2bcad91ba88108ef60048eaef8e7
Carlos Gerardo Enríquez Valerio <carlos@MacBook-Pro-de-Carlos.local>
2025-01-19 18:24:32 -0600

    Refactorization

--- SlotMachine/Assets/Scripts/Controllers/SlotMachineController.cs
+++ SlotMachine/Assets/Scripts/Controllers/SlotMachineController.cs
@@ -4,45 +4,69 @@ using Controllers;
 using Data.Combination;
 using Data.ReelData;
 using Gameplay.Controllers;
-using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// Controls the slot machine's behavior, including spinning the reels, checking combinations, and updating the score.
+/// </summary>
 public class SlotMachineController : MonoBehaviour
 {
+
+    [Tooltip(" Collection of possible winning combinations.")]
     [SerializeField] private CombinationCollectionDataSO _combinationCollection;
+    [Tooltip(" Reference data collection for reel items.")]
     [SerializeField] private ReelDataCollectionSO _referenceDataCollection;
+    [Tooltip("Array of reel controllers managing individual reels.")]
     [SerializeField] private ReelController[] _reelControllers;
+    [Tooltip("Delay between interactions with reels.")]
     [SerializeField] private float _reelInteractionDelay = 0.5f;
+    [Tooltip(" UI element displaying the current score.")]
     [SerializeField] private TMPro.TextMeshProUGUI _scoreText;
+    [Tooltip(" Button to start spinning the slot machine.")]
     [SerializeField] private Button _spinButton;
+
+    /// <summary>
+    /// Total score accumulated by the player.
+    /// </summary>
     private float _totalScore = 0;
+
+    /// <summary>
+    /// Initializes the slot machine controller and sets the initial score text.
+    /// </summary>
     private void Start()
     {
         _scoreText.text = $"Score: {_totalScore}";
     }
-    [ContextMenu("Start Spinning")]
+
+    /// <summary>
+    /// Starts the slot machine by disabling the spin button and initiating the spinning process.
+    /// </summary>
     public void StartMachine()
     {
         _spinButton.interactable = false;
         StartCoroutine(StartSpinning());
     }
-    [ContextMenu("Start Spinning Infinite")]
-    public void StartSpinningTestInfinite()
-    {
-        StartCoroutine(SetReelSatte(ReelState.Spinning));
-    }
+
+    /// <summary>
+    /// Coroutine that handles the spinning process of the slot machine.
+    /// </summary>
+    /// <returns>An IEnumerator for coroutine handling.</returns>
     public IEnumerator StartSpinning()
     {
         float spinDuration = Random.Range(2f, 4f);
 
-        yield return SetReelSatte(ReelState.Spinning);
+        yield return SetReelState(ReelState.Spinning);
         yield return new WaitForSeconds(spinDuration);
         yield return SetReelState(ReelState.Stopped, ReelState.Finished);
 
         yield return CheckCombinations();
     }
 
+    /// <summary>
+    /// Coroutine that checks for winning combinations and updates the score accordingly.
+    /// </summary>
+    /// <returns>An IEnumerator for coroutine handling.</returns>
     private IEnumerator CheckCombinations()
     {
         ReelItemController[][] reelItems = new ReelItemController[5][];
@@ -58,10 +82,8 @@ public class SlotMachineController : MonoBehaviour
         int totalScore = 0;
         for (int i = 0; i < 3; i++)
         {
-
             if (_combinationCollection.CheckCombinations((CombinationType)i, reelItems, out int newWinningLevel, out List<ReelItemController> winningItems))
             {
-
                 Debug.Log($"New winning level: {newWinningLevel} in line {(CombinationType)i}");
                 ReelItem winningReelItem = winningItems[0].ReelItem;
                 if (winningReelItem == ReelItem.Lemon && newWinningLevel - 1 == 1)
@@ -72,7 +94,6 @@ public class SlotMachineController : MonoBehaviour
                 {
                     item.ShowCombinationSuccess(true);
                 }
-
             }
             yield return new WaitForSeconds(1f);
         }
@@ -83,12 +104,21 @@ public class SlotMachineController : MonoBehaviour
         _spinButton.interactable = true;
     }
 
+    /// <summary>
+    /// Stops the spinning of the slot machine reels.
+    /// </summary>
     [ContextMenu("Stop Spinning")]
     public void StopSpinning()
     {
         StartCoroutine(SetReelState(ReelState.Stopped, ReelState.Finished));
     }
-    private IEnumerator SetReelSatte(ReelState state)
+
+    /// <summary>
+    /// Coroutine that sets the state of each reel with a delay between each interaction.
+    /// </summary>
+    /// <param name="state">The state to set for each reel.</param>
+    /// <returns>An IEnumerator for coroutine handling.</returns>
+    private IEnumerator SetReelState(ReelState state)
     {
         foreach (var reelController in _reelControllers)
         {
@@ -96,6 +126,13 @@ public class SlotMachineController : MonoBehaviour
             yield return new WaitForSeconds(_reelInteractionDelay);
         }
     }
+
+    /// <summary>
+    /// Coroutine that sets the state of each reel and waits until the reel reaches a specified condition state.
+    /// </summary>
+    /// <param name="state">The state to set for each reel.</param>
+    /// <param name="conditionState">The state to wait for each reel to reach.</param>
+    /// <returns>An IEnumerator for coroutine handling.</returns>
     private IEnumerator SetReelState(ReelState state, ReelState conditionState)
     {
         foreach (var reelController in _reelControllers)
